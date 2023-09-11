2c911031cdccbd59fb50dcd95d53b0eb43a3bad7
Ellis Ng'ona <66230306+mwanawabangona@users.noreply.github.com>
2023-09-11 08:58:48 -0700

    Added background Image

--- lib/pages/register_screen.dart
+++ lib/pages/register_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:laundryapp/utils/constants.dart';
 
 class registerScreen extends StatefulWidget {
   const registerScreen({super.key});
@@ -12,7 +13,24 @@ class _registerScreenState extends State<registerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Constants.primaryColor,
+      body: SafeArea(child: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              right: 0.0,
+              top: -20.0,
+              child: Opacity(
+                opacity: 0.3,
+                child: Image.asset(
+                  "assets/images/washing_machine_illustration.png",
+                ),
+              ),
+            )
+          ],
+        ),
+      ),),
     );
   }
 }
